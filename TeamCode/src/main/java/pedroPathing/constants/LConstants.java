d27d793962dc1669f47b64cbf1a5f9b2925edbbd
Tirth Shah <tirthsh@ataarobotics.ca>
2025-05-09 20:53:31 -0600

    Pedro pathing day 1 (naming motor names and odo pod)

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -2,10 +2,11 @@ package pedroPathing.constants;
 
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+       /* ThreeWheelConstants.forwardTicksToInches = .001989436789;
         ThreeWheelConstants.strafeTicksToInches = .001989436789;
         ThreeWheelConstants.turnTicksToInches = .001989436789;
         ThreeWheelConstants.leftY = 1;
@@ -17,6 +18,19 @@ public class LConstants {
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+
+        */
+
+        TwoWheelConstants.forwardTicksToInches = .001989436789;
+        TwoWheelConstants.strafeTicksToInches = .001989436789;
+        TwoWheelConstants.forwardY = 7;
+        TwoWheelConstants.strafeX = 0;
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "suck";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "par";
+        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        TwoWheelConstants.IMU_HardwareMapName = "imu";
+        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
     }
 }
 
